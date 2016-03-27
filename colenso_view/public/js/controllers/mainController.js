a01bda1292b3147f5df3f09a4e5d92ade8ec4cf6
Jah Seng Lee <jahseng619@gmail.com>
2016-03-28 00:08:25 +1300

    Added non-working basex code in searchService

--- colenso_view/public/js/controllers/mainController.js
+++ colenso_view/public/js/controllers/mainController.js
@@ -2,12 +2,12 @@ angular.module('myApp.controllers').controller('MainCtrl',
   ['$scope',
     '$http',
     function($scope, $http) {
-  $scope.searchBox = {searchString: "Test"};
+  $scope.searchBox = {searchString: ""};
   
   $scope.search = function() {
     // if string is not empty/spaces
     if($scope.searchBox.searchString.match(/^\s*$/) === null) {
-      // TODO: send this to BaseX backend
+      
     }
   }
 }]);
