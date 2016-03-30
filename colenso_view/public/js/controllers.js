2c54e1493ada7df2d434a5b6a7e39cf326645f4a
Jah Seng Lee <jahseng619@gmail.com>
2016-03-31 02:57:13 +1300

    Removed some old boilercode from the seed

--- colenso_view/public/js/controllers.js
+++ colenso_view/public/js/controllers.js
@@ -5,17 +5,6 @@
 angular.module('myApp.controllers', []).
   controller('AppCtrl', ['$scope', '$http', function($scope, $http){
 
-    $http({
-      method: 'GET',
-      url: '/api/name'
-    }).
-    success(function (data, status, headers, config) {
-      $scope.name = data.name;
-    }).
-    error(function (data, status, headers, config) {
-      $scope.name = 'Error!';
-    });
-
   }]);
     // .controller('MainCtrl', ['$scope', '$http', function($scope, $http){
     //   // Testing
